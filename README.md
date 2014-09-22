b55201c8ebda6ec17b67963a5ae95afb8f061e7a
Eric Schrijver <eric@ericschrijver.nl>
2014-09-22 23:23:30 +0200

    Comments & Documentation

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Comment Spam
 
 An example of a simple Cordova/PhoneGap App for the GOVT4 ‘Coding’ exercise ‘Poetic Spam Device’
 
+Uses the template so graciously left behind by a spammer on Scot Hanselman’s
+blog, who himself was so gracious to [archive it on GitHub](https://gist.github.com/shanselman/5422230).
--- www/js/index.js
+++ www/js/index.js
@@ -4,18 +4,42 @@ var pickRandom = function(list) {
 }
 
 var injectSpam = function() {
-    var replacer = function(match, p1, p2, p3, offset, string){
+    // this function will come in handy:
+    // it takes a string like appropriate|perfect|the best
+    // and outputs one of the three
+    var chooseWord = function(match, p1){
         var parts = p1.split('|');
         return pickRandom(parts);
     }
     
+    // we get the contents of the <pre>…</pre> block
     var pre = document.querySelector('pre');
     var spamTemplate = pre.innerHTML;
-    spamTemplate = spamTemplate.replace(/{([^}]*)}/g, replacer);
     
+    // this is the tricky bit, it’s where we fill in the template
+    
+    // first we get all the bits between curly brackets { }
+    // like: {appropriate|perfect|the best}
+    // using a technique known as ‘regular expression’
+    
+    // these bits we then pass through the function `chooseWord`
+    // which makes sure to pick one of three words
+    
+    // this word is what ends up replacing the original { } part
+    
+    // more on the `replace` function: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace
+    spamTemplate = spamTemplate.replace(/{([^}]*)}/g, chooseWord);
+    
+    // we filled in the entire template,
+    // but we just need one of the messages.
+    
+    // the different spam messages themselves are also separated by `|`
+    // so we split them up and randomly pick one
     var messages = spamTemplate.split("|");
     var message = pickRandom(messages);
     
+    // the <pre>…</pre> block gets the generated spam message
+    // as its new contents
     pre.innerHTML = message;
 }
 

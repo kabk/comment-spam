d3e6582f1b58cf6dd7b52eb59204e2d4b007b133
Julian <scheepsrecht@me.com>
2015-01-12 16:08:06 +0100

    iOS update

--- platforms/ios/Comment Spam/Classes/MainViewController.h
+++ platforms/ios/Comment Spam/Classes/MainViewController.h
@@ -19,7 +19,7 @@
 
 //
 //  MainViewController.h
-//  KABKapp
+//  Comment Spam
 //
 //  Created by ___FULLUSERNAME___ on ___DATE___.
 //  Copyright ___ORGANIZATIONNAME___ ___YEAR___. All rights reserved.
